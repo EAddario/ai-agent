cc27e3b907cbd14385a476a43fc6b05368b605c5
Ed Addario <eaddario@hotmail.com>
2025-01-21 16:59:12 +0000

    Add entry point

---
+++ index.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import { runAgent } from './src/agent.ts';
+import { tools } from './src/tools/index.js';
+
+dotenv.config();
+const userMessage = process.argv[2];
+
+if (!userMessage) {
+  console.error('Please provide a message');
+  process.exit(1);
+}
+
+const messages = await runAgent({
+  userMessage,
+  tools,
+});
