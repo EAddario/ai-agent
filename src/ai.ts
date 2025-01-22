885fc30808093b5d6f2d11301c874918cb5c7df1
Ed Addario <eaddario@hotmail.com>
2025-01-22 11:08:51 +0000

    Remove HTTP referrer

--- src/ai.ts
+++ src/ai.ts
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+
 dotenv.config();
 
 export const openai = new OpenAI({
   baseURL: process.env.AI_BASE_URL,
   apiKey: process.env.AI_API_KEY,
   defaultHeaders: {
-    "HTTP-Referer": 'https://github.com/EAddario/askhal',
     "X-Title": 'AI Agent'
   }
 });
