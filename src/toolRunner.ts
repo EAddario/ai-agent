5d4fdfe89cf74ec98753240035b8a0204e039cb7
Ed Addario <eaddario@hotmail.com>
2025-01-25 16:34:44 +0000

    Add toolApprovalRequired

--- src/toolRunner.ts
+++ src/toolRunner.ts
@@ -1,10 +1,10 @@
 import type OpenAI from 'openai';
-import {chuckNorrisJoke} from './tools/chuckNorrisJoke.ts';
-import {dadJoke} from './tools/dadJoke.ts';
-import {movieSearch} from './tools/movieSearch.ts';
-import {myIP} from './tools/myIP.ts';
-import {myLocation} from './tools/myLocation.ts';
-import {reddit} from './tools/reddit.ts';
+import {chuckNorrisJoke, chuckNorrisJokeToolDefinition} from './tools/chuckNorrisJoke.ts';
+import {dadJoke, dadJokeToolDefinition} from './tools/dadJoke.ts';
+import {movieSearch, movieSearchToolDefinition} from './tools/movieSearch.ts';
+import {myIP, myIPToolDefinition} from './tools/myIP.ts';
+import {myLocation, myLocationToolDefinition} from './tools/myLocation.ts';
+import {reddit, redditToolDefinition} from './tools/reddit.ts';
 
 export const runTool = async (
     toolCall: OpenAI.Chat.Completions.ChatCompletionMessageToolCall,
@@ -30,6 +30,34 @@ export const runTool = async (
 
         case 'reddit':
             return reddit(input);
+
+        default:
+            throw new Error(`Unknown tool: ${toolCall.function.name}`);
+    }
+}
+
+export const toolApprovalRequired = async (
+    toolCall: OpenAI.Chat.Completions.ChatCompletionMessageToolCall
+) => {
+
+    switch (toolCall.function.name) {
+        case 'chuck_norris_joke':
+            return chuckNorrisJokeToolDefinition.hitlApproval;
+
+        case 'dad_joke':
+            return dadJokeToolDefinition.hitlApproval;
+
+        case 'movie_search':
+                return movieSearchToolDefinition.hitlApproval;
+
+        case 'my_ip':
+            return myIPToolDefinition.hitlApproval;
+
+        case 'my_location':
+            return myLocationToolDefinition.hitlApproval;
+
+        case 'reddit':
+            return redditToolDefinition.hitlApproval;
 
         default:
             throw new Error(`Unknown tool: ${toolCall.function.name}`);
