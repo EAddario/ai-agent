87bbb1bd959a3212a54f14e2c969f0f0b504c4a2
Ed Addario <eaddario@hotmail.com>
2025-01-27 17:02:56 +0000

    Add memory management

--- src/memory.ts
+++ src/memory.ts
@@ -1,6 +1,10 @@
 import {JSONFilePreset} from 'lowdb/node';
+import { summarizeMessages } from './llm.ts';
 import {v4 as uuidv4} from 'uuid';
 import type {AIMessage} from '../types.ts';
+
+const messageWindowSize = 5;
+const messageSummarySize = 10;
 
 export type MessageWithMetadata = AIMessage & { id: string, createdAt: string }
 
@@ -16,8 +20,15 @@ export const removeMetadata = (message: MessageWithMetadata): AIMessage => {
     return messageWithoutMetadata;
 }
 
-type Data = { messages: MessageWithMetadata[] }
-const defaultData: Data = {messages: []}
+type Data = {
+    summary: string,
+    messages: MessageWithMetadata[]
+}
+
+const defaultData: Data = {
+    summary: '',
+    messages: []
+}
 
 export const getDb = async () => {
 
@@ -27,13 +38,33 @@ export const getDb = async () => {
 export const addMessages = async (messages: AIMessage[]) => {
     const db = await getDb();
     db.data.messages.push(...messages.map(addMetadata));
+
+    if (db.data.messages.length >= messageSummarySize) {
+        const oldestMessages = db.data.messages.slice(0, messageWindowSize).map(removeMetadata);
+        db.data.summary = await summarizeMessages(oldestMessages) as string;
+    }
+
     await db.write();
 }
 
 export const getMessages = async () => {
     const db = await getDb();
+    const messages = db.data.messages.map(removeMetadata);
+    const lastMessages = messages.slice(-messageWindowSize);
 
-    return db.data.messages.map(removeMetadata);
+    if (lastMessages[0]?.role === 'tool') {
+        const additionalMessage = messages[messages.length - (messageWindowSize + 1)];
+        if (additionalMessage) {
+            return [...[additionalMessage], ...lastMessages]
+        }
+    }
+
+    return lastMessages;
+}
+
+export const getSummary = async () => {
+    const db = await getDb()
+    return db.data.summary
 }
 
 export const saveToolResponse = async (
@@ -52,6 +83,6 @@ export const clearLIFOMessages = async (removeLast?: number) => {
 
 export const resetMemory = async () => {
     const db = await getDb();
-    db.data.messages = [];
+    db.data = defaultData;
     await db.write();
 }
