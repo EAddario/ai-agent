b22649d86af6bf683397c7e9158be708eef1a18d
Ed Addario <eaddario@hotmail.com>
2025-02-07 18:56:29 +0000

    Add year parameter

--- src/tools/movieSearch.ts
+++ src/tools/movieSearch.ts
@@ -7,6 +7,7 @@ const toolDescription = `
 This tool searches for movies and information about them, including title, genre, director, actors, year, running time, rating, number of votes, revenue, and description.
 This tool must always be used whenever the user asks or refers to information about movies.
 This tool must never be used if the user doesn't specifically asks for information about movies.
+This tool must only be used once per user query.
 `;
 
 export const movieSearchToolDefinition = {
@@ -15,19 +16,21 @@ export const movieSearchToolDefinition = {
     description: toolDescription,
     parameters: z.object({
         query: z.string().describe('The search query for finding movies'),
-        genre: z.string().optional().describe('Filter movies by genre'),
-        director: z.string().optional().describe('Filter movies by director')
+        genre: z.string().optional().describe('Filter movies by genre only if a genre is provided. Otherwise do not use this filter'),
+        director: z.string().optional().describe('Filter movies by director only if a director is provided. Otherwise do not use this filter'),
+        year: z.number().optional().describe('Filter movies by year only if a year is provided. Otherwise do not use this filter'),
     })
 }
 
 type Args = z.infer<typeof movieSearchToolDefinition.parameters>;
 
 export const movieSearch: ToolFn<Args, string> = async ({toolArgs}) => {
-    const {query, genre, director} = toolArgs;
+    const {query, genre, director, year} = toolArgs;
 
     const filters = {
         ...(genre && {genre}),
-        ...(director && {director})
+        ...(director && {director}),
+        ...(year && {year})
     }
 
     let results;
